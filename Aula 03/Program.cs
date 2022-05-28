dea0251d6b567c0156af373619ebd991ea2cefa3
Msfasolin <msfasolin@gmail.com>
2022-05-28 02:29:51 -0300

    Add valor operadores e valor horario.

--- Aula 03/Program.cs
+++ Aula 03/Program.cs
@@ -23,10 +23,76 @@ namespace Aula_03
                     Console.WriteLine("F- <=");
                     String escolherOpe = Console.ReadLine();
 
+                    switch (escolherOpe)
+                    {
+                        case "A":
+                            Console.WriteLine("Qual numero de X");
+                            int x = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int y = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(x == y);
+                            break;
+
+                        case "B":
+                            Console.WriteLine("Qual numero de X");
+                            int n1 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int n2 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(n1 != n2);
+                            break;
+                        case "C":
+                            Console.WriteLine("Qual numero de X");
+                            int n3 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int n4 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(n3 == n4);
+                            break;
+                        case "D":
+                            Console.WriteLine("Qual numero de X");
+                            int n5 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int n6 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(n5 == n6);
+                            break;
+                        case "E":
+                            Console.WriteLine("Qual numero de X");
+                            int n7 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int n8 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(n7 == n8);
+                            break;
+                        case "F":
+                            Console.WriteLine("Qual numero de X");
+                            int n9 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Qual numero de Y");
+                            int n10 = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine(n9 == n10);
+                            break;
+                        default:
+                            return;
+                    }
+
+                    break;
+
+                case 2:
+                    Console.WriteLine("Qual seu horario?");
+                    int time = Convert.ToInt32(Console.ReadLine());
+                    if (time < 12)
+                    {
+                        Console.WriteLine("Bom dia");
+                    }
+                    if (time <= 17)
+                    {
+                        Console.WriteLine("Boa Tarde");
+                    }
+                    if (time <= 18)
+                    {
+                        Console.WriteLine("Boa Noite");
+                    }
                     break;
 
 
-   
+
                 default:
                     Console.WriteLine("Não existe números");
                     break;
